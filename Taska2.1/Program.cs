7b099a242695c0be661babfcce6d02e63d2c1166
Olga <workpost.mkh@yandex.ru>
2023-08-03 16:17:55 +0300

    Taska 2.1

---
+++ Taska2.1/Program.cs
@@ -0,0 +1,5 @@
+﻿// 
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+string[] days = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+Console.WriteLine(days[number -1]);
