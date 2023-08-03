774373e45ea4a967e5b378b1541ac5fdfbc951fa
Olga <workpost.mkh@yandex.ru>
2023-08-03 18:06:20 +0300

    Taska 4

---
+++ Taska4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа. 456 -> 6 782 -> 2 918 -> 8
+//для предпоследнего следует сначала поделить на 10, а после деление без остатка на 10
+
+Console.WriteLine("Введите трехзначно число n");
+int number = int.Parse(Console.ReadLine()!);
+int firstDigit = number / 100 ; 
+int secondDigit = (number / 10) % 10 ; 
+int thirdDigit = number % 10 ; 
+
+Console.WriteLine($"Первая цифра в числе {number} равна {firstDigit}"); 
+Console.WriteLine($"Вторая цифра в числе {number} равна {secondDigit}");
+Console.WriteLine($"Третья цифра в числе {number} равна {thirdDigit}");
