186490dd46f68970055c00f4cc0618c25b79e2a8
Olga <workpost.mkh@yandex.ru>
2023-08-03 19:00:57 +0300

    Taska 4.1

---
+++ Taska4.1/Program.cs
@@ -0,0 +1,12 @@
+﻿// вариант задачи №4
+Console.WriteLine("Введите четырехзначное число n");
+int number = int.Parse(Console.ReadLine()!);
+int firstDigit = number / 1000 ; 
+int secondDigit = (number / 100) % 10 ; 
+int thirdDigit = (number / 10) % 10 ;  
+int fourDigit = number % 10 ;
+
+Console.WriteLine($"Первая цифра в числе {number} равна {firstDigit}"); 
+Console.WriteLine($"Вторая цифра в числе {number} равна {secondDigit}");
+Console.WriteLine($"Третья цифра в числе {number} равна {thirdDigit}");
+Console.WriteLine($"Четвертая цифра в числе {number} равна {fourDigit}");
