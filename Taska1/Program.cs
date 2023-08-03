a3e217fccc9cfdc5d116ff7672ca9d1eb127cd28
Olga <workpost.mkh@yandex.ru>
2023-08-03 10:41:33 +0300

    Taska 1

---
+++ Taska1/Program.cs
@@ -0,0 +1,19 @@
+﻿// //Напишите программу, которая на вход принимает два //числа и проверяет, является ли первое число //квадратом второго. 
+//a = 25, b = 5 -> да 
+//a = 2, b = 10 -> нет 
+//a = 9, b = -3 -> да 
+//a = -3 b = 9 -> нет
+
+Console.Write("Введите первое целое число: "); 
+int iNumberA = int.Parse(Console.ReadLine()!); 
+Console.Write("Введите второе целое число: "); 
+int iNumberB = int.Parse(Console.ReadLine()!); 
+if (iNumberA / iNumberB == iNumberB)
+ {
+ Console.WriteLine($"Условие выполнено: {iNumberA} является квадратом {iNumberB}"); 
+} 
+else 
+{ 
+Console.WriteLine($"Условие не выполнено: {iNumberA} не является квадратом {iNumberB}"); 
+}
+
